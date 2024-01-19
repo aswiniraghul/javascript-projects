5867b673e891916e56698e6ea3d353bebfc6ada1
Aswini Raghul <aswiniraghul@gmail.com>
2024-01-18 23:07:22 -0600

    Studio Strings and Arrays

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -11,8 +11,6 @@ function reverseCommas() {
 	//TODO: 1. create and instantiate your variables.
 	let check;
 	let output;
-	let strArray = [];
-	let reversedString = "";
 	//TODO: 2. write the code required for this step
 
 	if (protoArray1.includes(",")) {
@@ -26,9 +24,9 @@ function reverseCommas() {
 	}
 
 	if (protoArray1.includes(",")) {
-		strArray = protoArray1.split(",");
-		strArray.reverse();
-		reversedString = strArray.join(",");
+		check = protoArray1.split(",");
+		check.reverse();
+		output = check.join(",");
 	}
 
 	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
@@ -40,8 +38,6 @@ function semiDash() {
 
 	let check;
 	let output;
-	let strArray = [];
-	let reversedString = "";
 	//TODO: write the code required for this step
 
 	if (protoArray2.includes(",")) {
@@ -55,9 +51,9 @@ function semiDash() {
 	}
 
 	if (protoArray2.includes(";")) {
-    	strArray = protoArray2.split(";");
-    	strArray.sort();
-    	reversedString = strArray.join("-");
+    	check = protoArray2.split(";");
+    	check.sort();
+    	output = check.join("-");
 	}
   
 	return output;
@@ -68,8 +64,6 @@ function reverseSpaces() {
 
 	let check;
 	let output;
-	let strArray = [];
-	let reversedString = "";
   //TODO: write the code required for this step
 
 	if (protoArray3.includes(",")) {
@@ -83,9 +77,9 @@ function reverseSpaces() {
 	}
 
 	if (protoArray3.includes(" ")) {
-		strArray = protoArray3.split(" ");
-		strArray.sort((a, b) => b.localeCompare(a)); //Ask reverse alphabet
-		reversedString = strArray.join(" ");
+		check = protoArray3.split(" ");
+		output = check.sort().reverse();
+		output = check.join(" ");
 	}
 
 	return output;
@@ -96,8 +90,6 @@ function commaSpace() {
 
 	let check;
 	let output;
-	let strArray = [];
-	let reversedString = "";
 	//TODO: write the code required for this step
 
 	if (protoArray4.includes(",")) {
@@ -111,9 +103,9 @@ function commaSpace() {
 	}
 
 	if (protoArray4.includes(", ")) {
-		strArray = protoArray4.split(", "); //Ask what is part b
-		strArray.reverse();
-		reversedString = strArray.join(",");
+		check = protoArray4.split(", "); //Ask what is part b
+		check.reverse();
+		output = check.join(",");
 	}
   
 	return output;
