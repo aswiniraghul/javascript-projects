d9f2988719bfdf85bba26c53ab0107d432830676
Aswini Raghul <aswiniraghul@gmail.com>
2024-01-31 19:02:09 -0600

    completed objects and math Exercises

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -1,16 +1,59 @@
+
 let superChimpOne = {
    name: "Chad",
    species: "Chimpanzee",
    mass: 9,
-   age: 6
+   age: 6,
+   astronautID: 1,
+   move: function () {return Math.floor(Math.random()*11)}
 };
 
 let salamander = {
    name: "Lacey",
    species: "Axolotl Salamander",
    mass: 0.1,
-   age: 5
+   age: 5,
+   astronautID: 2,
+   move: function () {return Math.floor(Math.random()*11)}
 };
+
+let superChimpTwo = {
+   name: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 6,
+   astronautID: 3,
+   move: function () {return Math.floor(Math.random()*11)}
+};
+
+let dog = {
+   name: "Leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 5,
+   astronautID: 4,
+   move: function () {return Math.floor(Math.random()*11)}
+};
+
+let waterBear = {
+   name: "Almina",
+   species: "Tardigrade",
+   mass: 0.0000000001,
+   age: 1,
+   astronautID: 5,
+   move: function () {return Math.floor(Math.random()*11)}
+};
+
+let crew = [superChimpOne, superChimpTwo, salamander, dog, waterBear];
+
+function crewReports(animal) {
+   return `${animal.name} is a ${animal.species}. They are ${animal.age} years old and ${animal.mass} kilograms. Their ID is ${animal.astronautID}.`;
+}
+
+// Example usage:
+for (let i = 0; i < crew.length; i++) {
+   console.log(crewReports(crew[i]));
+}
 
 
 // After you have created the other object literals, add the astronautID property to each one.
@@ -20,3 +63,28 @@ let salamander = {
 // Print out the relevant information about each animal.
 
 // Start an animal race!
+function fitnessTest(animals) {
+   let raceResults = [];
+
+   for (let i = 0; i < animals.length; i++) {
+       let steps = 0;
+       let turns = 0;
+
+       while (steps < 20) {
+           steps += animals[i].move();
+           turns++;
+       }
+
+       raceResults.push(`${animals[i].name} took ${turns} turns to take 20 steps.`);
+   }
+
+   return raceResults;
+}
+
+// Example usage:
+let results = fitnessTest(crew);
+
+// Print the results to the console
+for (let i = 0; i < results.length; i++) {
+   console.log(results[i]);
+}
