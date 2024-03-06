c548339e46d24d02d65aeeff573a068827cc1ae3
Aswini Raghul <aswiniraghul@gmail.com>
2024-03-05 23:12:54 -0600

    Fetch studio modification

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,36 +1,37 @@
 //TODO: Add Your Code Below
 window.addEventListener('load', function () {
   const container = document.getElementById('container');
+  // fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function(response) {
+  //  response.json().then(function(data){
+  //   for (let i=0; i < data.length; i++) {
+  //           let astronaut = data[i];
+  //           container.innerHTML += `
+  //             <div class="astronaut">
+  //               <div class="bio">
+  //                 <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+  //                 <ul>
+  //                   <li>Hours in space: ${astronaut.hoursInSpace}</li>
+  //                   <li>Active: ${astronaut.active}</li>
+  //                   <li>Skills: ${astronaut.skills.join(", ")}</li>
+  //                 </ul>
+  //               </div>
+  //               <img class="avatar" src="${astronaut.picture}">
+  //              </div>
+  //            `
+  //   }  
+  //  });
+
+  // });
   fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function(response) {
-   response.json().then(function(data){
-    for (let i=0; i < data.length; i++) {
-            let astronaut = data[i];
-            container.innerHTML += `
-              <div class="astronaut">
-                <div class="bio">
-                  <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
-                  <ul>
-                    <li>Hours in space: ${astronaut.hoursInSpace}</li>
-                    <li>Active: ${astronaut.active}</li>
-                    <li>Skills: ${astronaut.skills.join(", ")}</li>
-                  </ul>
-                </div>
-                <img class="avatar" src="${astronaut.picture}">
-               </div>
-             `
-     }     
-   });
-
-
-    
+    response.json().then(function(data){
      //Bonus 1  
        data.sort(function (a, b) {
         return a.hoursInSpace < b.hoursInSpace ? 1 : -1;
-      });
-     
+      
+       });
     //  Bonus 3
     const count = document.getElementById('count');
-    count.innerHTML = `These ${data.length} for the Astronauts`;
+    count.innerHTML = `The Astronaut count is : ${data.length}. `;
 
       for (let i = 0; i < data.length; i++) {
         let astronaut = data[i];
@@ -51,8 +52,11 @@ window.addEventListener('load', function () {
           <img class="avatar" src="${astronaut.picture}">
         </div>
       `;
-    
       }
+      
     });
   
-  }); 
+  });
+    
+    
+  });
